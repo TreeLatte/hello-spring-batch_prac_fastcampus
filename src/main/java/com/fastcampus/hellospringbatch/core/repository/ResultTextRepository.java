142c7d46bc05bbc83c247592cb144a6fcef1bf01
treelatte <jjh8623@gmail.com>
2023-08-17 18:23:25 +0900

    기본적인 spring batch(tasklet방식)과 processor,reader,writer를 쓰는 방식(Chunk)의 구현 + 테스트 부분까지

---
+++ src/main/java/com/fastcampus/hellospringbatch/core/repository/ResultTextRepository.java
@@ -0,0 +1,11 @@
+package com.fastcampus.hellospringbatch.core.repository;
+
+import com.fastcampus.hellospringbatch.core.domain.PlainText;
+import com.fastcampus.hellospringbatch.core.domain.ResultText;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultTextRepository extends JpaRepository<ResultText, Integer> {
+
+}
